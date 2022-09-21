68986f78f3bfed960343ad4c53dfbd49bb20f67b
Manuelevaristo <findaevaristo@gmail.com>
2022-09-20 22:47:48 -0300

    endpoint read all+ read By Id

--- todo-list/src/components/Todo.jsx
+++ todo-list/src/components/Todo.jsx
@@ -1,16 +1,45 @@
-import React from "react";
+import "./Todo.css";
+import React, { useState } from "react";
 
 function Todo() {
+  const [task, setTask] = useState("");
+  const [itemsList, setItemsList] = useState([]);
+
+  function handleChangeInput(event) {
+    const inputTask = event.target.value;
+
+    setTask(inputTask);
+  }
+
+  function handleAddItemToList(event) {
+    event.preventDefault();
+
+    if (!task) {
+      return alert("preencha uma tarefa");
+    }
+
+    setItemsList([...itemsList, task]);
+    setTask("");
+  }
+
   return (
-    <div className="todo-wrapper">
-      <h1>ToDo List</h1>
-      <form>
-        <input type="text" placeholder="Adicione uma tarefa" />
+    <div className="todo-container">
+      <h1 className="title">ToDo List</h1>
+      <form onSubmit={handleAddItemToList}>
+        <input
+          type="text"
+          placeholder="Adicione uma tarefa"
+          onChange={handleChangeInput}
+          value={task}
+        />
+
         <button type="submit">Adicionar</button>
       </form>
 
       <ul className="todo-list">
-        <li>Item</li>
+        {itemsList.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
       </ul>
     </div>
   );
